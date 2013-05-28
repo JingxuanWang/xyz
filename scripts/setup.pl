e36a521b60a62d7c87b9cc9d1d11938fe2d124be
JingxuanWang <wang.jingxuan@dena.jp>
2013-05-28 19:38:43 +0900

    add build utility

---
+++ scripts/setup.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $DRYRUN = 0;
+my $PATH = "../js";
+my $TARGET_FILE = "game.js";
+my @FILE_LIST = qw/
+	config.js
+	chara.js
+	battle.js
+	entry.js
+/;
+
+sub main {
+	_runCmd("rm $PATH/$TARGET_FILE");
+	for my $file (@FILE_LIST) {
+		_runCmd("cat $PATH/$file >> $PATH/$TARGET_FILE");	
+	}
+}
+
+sub _runCmd {
+	my ($cmd) = @_;
+	print "Execute $cmd ...\n";
+	if (!$DRYRUN) {
+		`$cmd`;
+	}
+}
+
+&main();
+
+
