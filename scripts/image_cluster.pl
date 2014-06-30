2559aff3d45bed3e9d95dee2b26aec348930dfdc
JingxuanWang <wjx1985@gmail.com>
2014-07-01 00:04:22 +0900

    update vector file

--- scripts/image_cluster.pl
+++ scripts/image_cluster.pl
@@ -9,33 +9,35 @@ sub parse_vector {
 	my ($filename) = @_;
 	
 	chomp($filename);
-
-	if ($filename =~ /^r: (\d+), g: (\d+), b: (\d+).png$/) {
+	
+	print $filename,"\n";
+	if ($filename =~ /^r:(\d+), g:(\d+), b:(\d+).png$/) {
 		return +[$1, $2, $3];	
 	}
 }
 
 sub get_vectors {
 	my @filenames = `ls $DIR`;
-	return map { get_vector($_) } @filenames;
+	my @vectors = map { parse_vector($_) } @filenames;
+	return \@vectors;
 }
 
 sub dump_vectors {
 	my ($vectors) = @_;
 
-	open FILE ">vector.txt" or die "Can not open file";
+	open FILE, ">vector.txt" or die "Can not open file";
 
-	foreach my $vector (@$vectors) {
-		print FILE "$vector->[0] $vector[1] $vector[2]\n";
+	for my $vector (@$vectors) {
+		print FILE "$vector->[0] $vector->[1] $vector->[2]\n";1
 	}
 
 	close FILE;
 }
 
 sub main {
-	@vectors = get_vectors();
+	my $vectors = get_vectors();
 
-	dump_vectors(\@vectors);
+	dump_vectors($vectors);
 }
 
 &main();
